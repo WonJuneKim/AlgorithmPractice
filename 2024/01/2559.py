82fe5fd55807f2b3f956d7136b4a8f0255f7bd82
onejuneee <onejuneee721@gmail.com>
2024-01-17 23:10:45 +0900

    BJ_2559_1월17일_시간초과2

--- 2024/01/2559.py
+++ 2024/01/2559.py
@@ -1,9 +1,14 @@
+import sys
+input_sys = sys.stdin.readline
 n, k = map(int, input().split())
-answer = []
 b = list(map(int, input().split()))
+answer = 0
+current = 0
 
+# 여기서 n이 100000까지 될 수 있음
 for i in range(n-k+1):
-    answer.append(sum(b[i:i+k]))
+    current = sum(b[i:i+k])
+    answer = max(current, answer)
 
-print(max(answer))
+print(answer)
 
