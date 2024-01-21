b83e7a674feeb499ca9581973f5717bebb31d5d7
onejuneee <onejuneee721@gmail.com>
2024-01-21 23:48:38 +0900

    [240121] BOJ / 9375 / 일곱난쟁이

--- 2024/01/9375.py
+++ 2024/01/9375.py
@@ -6,3 +6,22 @@ for _ in range(a):
     n= int(input())
     for _ in range(n):
         cloth, name = 1
+
+#
+# t = int(input())
+#
+# for i in range(t):
+#     n = int(input())
+#     weardict = {}
+#     for j in range(n):
+#         wear = list(input().split())
+#         if wear[1] in weardict:
+#             weardict[wear[1]].append(wear[0])
+#         else:
+#             weardict[wear[1]] = [wear[0]]
+#
+#     cnt = 1 # 각 종류마다 항목의 개수
+#
+#     for k in weardict:
+#         cnt *= (len(weardict[k])+1)
+#     print(cnt-1)
